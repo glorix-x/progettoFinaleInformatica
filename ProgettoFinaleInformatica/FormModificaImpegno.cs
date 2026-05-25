c4a6a5b218c666da9a24b63e944f018667062226
glori <glori@gloria.(none)>
2026-05-25 10:20:23 +0200

    odioo+

--- ProgettoFinaleInformatica/FormModificaImpegno.cs
+++ ProgettoFinaleInformatica/FormModificaImpegno.cs
@@ -26,15 +26,21 @@ namespace ProgettoFinaleInformatica
 
             if(!(impegno is ImpegnoRicorrente)) {
                 Impegno prossimoImpegno = gestore.GetNextImpegno(giorno);
-                if(prossimoImpegno != null && prossimoImpegno.DataFissata.Date.CompareTo(giorno.Date) == 0) {
+                //Esiste un prossimo impegno e Verifica se è nello stesso giorno.
+                if (prossimoImpegno != null && prossimoImpegno.DataFissata.Date.CompareTo(giorno.Date) == 0) {
+                    //Prende la data/ora del prossimo impegno
                     DateTime oraProssimoImpegno = prossimoImpegno.DataFissata;
+                    //Calcola quante ore ci sono tra: l’impegno corrente il prossimo.
                     numOreMax = oraProssimoImpegno.Hour - giorno.Hour;
                 } else {
+                    //Calcola quante ore restano fino a mezzanotte  
                     numOreMax = 24 - giorno.Hour;
                 }
             } else {
+                //Cast impegno in tipo ImpegnoRicorrente
                 numOreMax = 24 - ((ImpegnoRicorrente)impegno).ImpegnoOrigine.DataFissata.Hour;
             }
+            //Controllo il numero massimo di ore consentite
             ucModifica.SetData(numOreMax);
         }
 
@@ -48,19 +54,22 @@ namespace ProgettoFinaleInformatica
 
         private void btEliminaM_Click(object sender, EventArgs e)
         {
+            //Controlla se è ricorrente.
             if (impegno is ImpegnoRicorrente)
             {
+                //Converte l’oggetto,elimina l’impegno originale
                 gestore.RimuoviImpegno(((ImpegnoRicorrente)impegno).ImpegnoOrigine);
             }
             else
             {
+                //Elimina direttamente l’impegno normale
                 gestore.RimuoviImpegno(impegno);
             }
             form.Aggiorna();
             form.Show();
             this.Close();
         }
-
+        //Imposta il colore di sfondo della finestra quando viene aperto
         private void FormModificaImpegno_Load(object sender, EventArgs e)
         {
             this.BackColor = ColorTranslator.FromHtml(impegno.ColoreHex);
--- ProgettoFinaleInformatica/UserControlModifica.cs
+++ ProgettoFinaleInformatica/UserControlModifica.cs
@@ -35,7 +35,7 @@ namespace ProgettoFinaleInformatica
             rtbDescrizioneM.Text = i.Descrizione;
             cbDurata.Text = i.DurataOre.ToString();
 
-            // ✅ Gestiamo la checkbox "Ripeti":
+            // Gestiamo la checkbox "Ripeti":
             // Se RipetutoOgni è > 0, l'impegno è ricorrente
             if (i.RipetutoOgni > 0)
             {
@@ -48,27 +48,27 @@ namespace ProgettoFinaleInformatica
                 tbRipeti.Text = "";
             }
         }
-
+        //Configura i controlli grafici
         public void SetData(int numOreMax)
         {
             tbTitoloM.Text = i.Titolo;
             rtbDescrizioneM.Text = i.Descrizione;
+            //Genera automaticamente una lista di numeri
             cbDurata.DataSource = Enumerable.Range(1, numOreMax).ToList();
-            if(i is ImpegnoRicorrente) {
+            //Seleziona nel ComboBox la durata originale
+            if (i is ImpegnoRicorrente) {
                 cbDurata.SelectedIndex = ((ImpegnoRicorrente)i).ImpegnoOrigine.DurataOre - 1;
             } else {
+                //Imposta la durata normale
                 cbDurata.SelectedIndex = i.DurataOre - 1;
             }
 
 
             cbColore.DataSource = listaColori;
             cbColore.DisplayMember = "Name";
+            //Converte una stringa HTML in un oggetto Color     
             Color colore = ColorTranslator.FromHtml(i.ColoreHex);
-            //cbColore.SelectedIndex = listaColori.IndexOf(listaColori.FirstOrDefault(c => colore.R == c.R && colore.G == c.G && colore.B == c.B));
-            cbColore.SelectedIndex = Array.IndexOf(
-                listaColori,
-                listaColori.FirstOrDefault(c => colore.R == c.R && colore.G == c.G && colore.B == c.B)
-            );
+            cbColore.SelectedIndex = listaColori.IndexOf(listaColori.FirstOrDefault(c => colore.R == c.R && colore.G == c.G && colore.B == c.B));
         }
 
         private void cbRipeti_CheckedChanged(object sender, EventArgs e)
@@ -88,18 +88,19 @@ namespace ProgettoFinaleInformatica
             Impegno impegno = null;
             if (i is ImpegnoRicorrente)
             {
+                //Modifica l’impegno originale
                 impegno = ((ImpegnoRicorrente)i).ImpegnoOrigine;
             }
             else
             {
                 impegno = i;
             }
-
+            //Aggiornamenti
             impegno.Titolo = tbTitoloM.Text;
             impegno.Descrizione = rtbDescrizioneM.Text;
             impegno.DurataOre = (int)cbDurata.SelectedValue;
             impegno.ColoreHex = ColoreHex(listaColori[cbColore.SelectedIndex]);
-
+            //Convertire il testo in numero senza errori , Salva il risultato nella variabile giorni
             if (cbRipeti.Checked && int.TryParse(tbRipeti.Text, out int giorni) && giorni > 0)
             {
                 impegno.RipetutoOgni = giorni;
